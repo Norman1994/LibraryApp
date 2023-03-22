a923c3f634bce1db65e6c1ed62a144bf79892647
Dmitriy <dima.kulikov1993@gmail.com>
2023-03-22 18:05:58 +0300

    Начата работа над авторизацией

---
+++ Library.API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate()
+        {
+            return null;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register()
+        {
+            return Ok();
+        }
+
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+    }
+}
